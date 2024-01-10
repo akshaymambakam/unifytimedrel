d19b8b27d05b7305ea18b3116795d5411b7c325d
akshay <akshaymambakam@gmail.com>
2024-01-10 11:35:32 +0100

    [SCALABILITY] Examples to test scalability of eitre tool wrt number of parameters.

--- runscript.sh
+++ runscript.sh
@@ -7,3 +7,8 @@ g++ test_box.cpp -lgmp -lgmpxx
 ./mptre porv.ptre real_signal.csv 3 porv_pos.txt porv_neg.txt
 ./mptre neg_porv.ptre real_signal.csv 3 porv_pos.txt porv_neg.txt
 ./mptre paramecg.ptre debugTest.csv 3 pos_ecg.txt neg_ecg.txt
+# EITRE Scalability test
+./eitre escaletest1.ptre testBlowup.csv 1
+./eitre escaletest3.ptre testBlowup.csv 3
+./eitre escaletest5.ptre testBlowup.csv 5
+./eitre escaletest7.ptre testBlowup.csv 7
